083cf48bb85c735745089d783c4449e12e1b1d93
LChestnut070 <lx2117764507@gmail.com>
2023-05-25 22:53:18 +0800

    完成了首页开发

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
+import {$http} from "@escook/request-miniprogram"
+
 Vue.config.productionTip = false
+
+uni.$http = $http;
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+// 数据请求之前
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title: "数据加载中..."
+	})
+}
+// 数据请求之后
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+// 数据请求失败,挂载自定义方法
+uni.$showErr = function(){
+	uni.showToast({
+		title: '数据加载失败',
+		duracation: 1500,
+		icon: 'none'
+	})
+}
 App.mpType = 'app'
 const app = new Vue({
   ...App
